133b79ab36ed609c667ce56002689b210f3ada3b
dananatsheh <natshehdana684@gmail.com>
2026-08-06 15:52:04 +0300

    edited main

--- project 7/src/main.cpp
+++ project 7/src/main.cpp
@@ -1,15 +1,20 @@
 #include <Arduino.h>
-#include<WiFi.h>
+#include <WiFi.h>
+#include "BME280Sensor.h"
 
 #define SSID       "CYBER_EXT"
 #define PASS       "cyberap2025"
 
-void setup() {
- Serial.begin(115200);
- delay(500);
- WiFi.begin(SSID, PASS);
+#define SENSOR_READ_INTERVAL_MS   2500
 
- while (WiFi.status() != WL_CONNECTED) {
+BME280Sensor bme280;
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  WiFi.begin(SSID, PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -23,10 +28,36 @@ void setup() {
 
   if (tcpOk) {
     Serial.println("TCP connect OK");
-} else {
+  } else {
     Serial.println("TCP connect FAILED");
-}
+  }
+
+  Wire.begin();
+  bool bmeOk = bme280.begin();
+  if (bmeOk) {
+    Serial.println("Initialized BME280");
+  } else {
+    Serial.println("BME280 failed to initialize");
+  }
 }
 
 void loop() {
+  static unsigned long lastRead = 0;
+
+  if (millis() - lastRead > SENSOR_READ_INTERVAL_MS) {
+    lastRead = millis();
+    bme280.readNow();
+
+    bool validity = bme280.isDataValid();
+    if (validity) {
+      Serial.print("Temperature: ");
+      Serial.println(bme280.getTemperature());
+      Serial.print("Humidity: ");
+      Serial.println(bme280.getHumidity());
+      Serial.print("Pressure: ");
+      Serial.println(bme280.getPressure());
+    } else {
+      Serial.println("Readings are invalid");
+    }
+  }
 }
